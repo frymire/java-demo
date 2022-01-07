4fa094a5d4d242bae8bae157901934559f1db984
Mark Frymire <mark.e.frymire@wbbinc.com>
2022-01-07 10:32:12 -0500

    Demo command line parameters

---
+++ src/main/java/CommandLineParameters.java
@@ -0,0 +1,17 @@
+
+public class CommandLineParameters {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Hi.");
+    
+    for(int i = 0; i < args.length; i++) {
+      
+      if(args[i].equals("--linux")) {
+        System.out.println("Linux!");
+      }
+      
+      System.out.println(i + ": " + args[i]);
+    }
+  }
+}
