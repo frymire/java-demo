53bf8bc2167f0a4064feba706abf93a0a1b6fa7e
Mark Frymire <mark.frymire@icloud.com>
2022-11-16 16:42:41 -0500

    Update gitignore for IntelliJ

---
+++ src/main/java/Scrap.java
@@ -0,0 +1,9 @@
+
+public class Scrap {
+  
+  public static void main(String[] args) {
+    String theString = "Hey, how are you doing?";
+    System.out.println(theString.replace(",", ""));
+  }
+
+}
