a61c90551b9e07b2e72b5dea6505f4b02b0c1ab9
Mark Frymire <mark.frymire@icloud.com>
2023-09-26 10:20:41 -0400

    Update iterables demo

--- src/main/java/datastructures/MultipleIterables.java
+++ src/main/java/datastructures/MultipleIterables.java
@@ -9,12 +9,12 @@ public class MultipleIterables implements Iterable<Integer> {
 	
 	// At some point, we might want to switch from an ArrayList (which is unsynchronized), 
 	// to a Vector (which is synchronized) without the client knowing about it.
-	private List<Integer> m_ints = new ArrayList<>();
+	private final List<Integer> m_ints = new ArrayList<>();
 //	private Vector<Integer> m_ints = new Vector<>();
 		
 	// We'd also like to be able to iterate over these
-	private List<Double> m_doubles = new ArrayList<>();
-	private List<String> m_strings = new ArrayList<>();
+	private final List<Double> m_doubles = new ArrayList<>();
+	private final List<String> m_strings = new ArrayList<>();
 	
 	public MultipleIterables() {		
 		m_ints.add(1);
@@ -29,13 +29,20 @@ public class MultipleIterables implements Iterable<Integer> {
 	}
 	
 	// We'll use the iterator for the whole class to provide access to the ints
-	@Override public Iterator<Integer> iterator() { return m_ints.iterator(); }
+	@Override
+	public Iterator<Integer> iterator() { return m_ints.iterator(); }
 
 	// Unfortunately, Java doesn't allow us to also add Iterable<Double> and Iterable<String> to the interface.  Instead, we
 	// define inner classes, each with the sole job of implementing one of the new Iterable types and providing an iterator()
 	// override the gives access to the desired data.
-	private class Doubles implements Iterable<Double> { @Override public Iterator<Double> iterator() { return m_doubles.iterator(); } }	
-	private class Strings implements Iterable<String> { @Override public Iterator<String> iterator() { return m_strings.iterator(); } }
+	private class Doubles implements Iterable<Double> {
+		@Override
+		public Iterator<Double> iterator() { return m_doubles.iterator(); }
+	}
+	private class Strings implements Iterable<String> {
+		@Override
+		public Iterator<String> iterator() { return m_strings.iterator(); }
+	}
 	
 	// Now, provide methods to allow clients to get an iterator to the data they want.  This is pretty tricky, because these 
 	// methods return the private classes Doubles and Strings.  As shown in the main method below, the clients won't be able 
@@ -46,22 +53,20 @@ public class MultipleIterables implements Iterable<Integer> {
 	public static void main(String[] args) {
 		
 		// Make an instance of the class, of course
-		MultipleIterables myCI = new MultipleIterables();
+		MultipleIterables iterables = new MultipleIterables();
 		
 		// Iterators allow clients to iterate over the private members without knowing 
 		// whether they're stored as a Vector or ArrayList.
-		for (Integer i: myCI) System.out.println(i);
+		for (Integer i: iterables) System.out.println(i);
 		
 		// Now, call the special iterator methods to be able to loop over the other collections
-		for (Double d: myCI.getDoubles()) System.out.println(d);
-		for (String s: myCI.getStrings()) System.out.println(s);		
+		for (Double d: iterables.getDoubles()) System.out.println(d);
+		for (String s: iterables.getStrings()) System.out.println(s);
 
-		// NOTE: even though the getXXX() methods work above in the context of a for-loop, a client can't do this, 
-		// because the inner classes are private.  (They happen to work here since I jammed the main into the class 
-		// itself, but that was just so I could keep the demo code in one place.)
-		//CustomIterables.Doubles myCIdoubles = myCI.getDoubles();
-		//CustomIterables.Strings myCIstrings = myCI.getStrings();
-
-	} // main
-	
-} // CustomIterables
+		// NOTE: even though the getXXX() methods work above in the context of a for-loop, a client generally can't do
+		// this, because the inner classes are private.  (They happen to work here since I jammed the main into the
+		// class itself, but that was just so I could keep the demo code in one place.)
+		MultipleIterables.Doubles doubles = iterables.getDoubles();
+		MultipleIterables.Strings strings = iterables.getStrings();
+	}
+}
